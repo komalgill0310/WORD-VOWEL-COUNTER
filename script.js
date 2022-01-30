4075bd09ae9fcb537f670f32eaa477e346fdb9ff
Komal Gill <komalgill0310@gmail.com>
2022-01-29 20:57:25 -0500

    count total number of words & vowels

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ let text = document.getElementById("paragraph-text");
 document.getElementById("count-word-vowel").addEventListener("click", (e) => {
   e.preventDefault();
   countVowels();
+  countConsonants();
+  countWords();
   text.value = "";
 });
 
 function countVowels() {
-  let i = text.value;
-  let x = /[aeiou]/gim;
-  let result = i.match(x);
-  let y = result.length;
-  console.log(result);
-  console.log(y);
-  document.getElementById("word-count").textContent = `Vowels: ${y}`;
+  const vowelCount = text.value.match(/[aeiou]/gim).length;
+  document.getElementById("vowel-count").textContent = `Vowels: ${vowelCount}`;
 }
 
+function countWords(){
+ let wordCount = text.value.match(/([^\s])+/gim).length;
+ document.getElementById("word-count").textContent = `Word: ${wordCount}`;
+}

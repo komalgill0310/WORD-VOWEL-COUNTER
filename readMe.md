c71688869a6c92349f161e73d81af2c235806507
Komal Gill <komalgill0310@gmail.com>
2022-01-30 13:41:08 -0500

    count vowel and word

---
+++ readMe.md
@@ -0,0 +1,21 @@
+# **Word-Vowel Counter App**
+
+<font size = 6> This application is simply written in ***JavaScript, HTML and CSS***</font>
+
+<font size=5> Live Version ***[here](https://komalgill0310.github.io/Pomodoro-Timer/)***</font>
+
+<img src = "app screenshot\work_Timer.png">
+
+<img src = "app screenshot\rest_Timer.png">
+
+# **Summary**
+<font size=5>
+
+* **Tools:** RegEx, addEventListener, getElementById
+* **How the app work?**
+  * After typing in the textBox, it displays the total number of words and vowels included in the text on the screen when the **count word&vowel button** is clicked. 
+  * Users can reset an app by clicking the **Reset button**.
+* It was a great project for getting a better understanding of **Regular Expressions**.
+
+# Author
+<font size=5>Komal - | Software Developer | Personl Website is in progress | [LinkedIn](www.linkedin.com/in/komalpreet-kaur-3b6924177)
--- script.js
+++ script.js
@@ -3,17 +3,26 @@ let text = document.getElementById("paragraph-text");
 document.getElementById("count-word-vowel").addEventListener("click", (e) => {
   e.preventDefault();
   countVowels();
-  countConsonants();
   countWords();
-  text.value = "";
 });
+
+document.getElementById("reset").addEventListener("click", (e) => {
+  e.preventDefault();
+resetPage();
+})
 
 function countVowels() {
   const vowelCount = text.value.match(/[aeiou]/gim).length;
-  document.getElementById("vowel-count").textContent = `Vowels: ${vowelCount}`;
+  document.getElementById("vowel-count").textContent = `Contains ${vowelCount}vowels`;
 }
 
 function countWords(){
  let wordCount = text.value.match(/([^\s])+/gim).length;
- document.getElementById("word-count").textContent = `Word: ${wordCount}`;
+ document.getElementById("word-count").textContent = `Contains ${wordCount}words`;
+}
+
+function resetPage(){
+  text.value = "";
+  document.getElementById("vowel-count").textContent = "";
+  document.getElementById("word-count").textContent = "";
 }

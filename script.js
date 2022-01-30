907a1ad0840ef6592a07b4e5e2f44c0ac3d7dbd2
Komal Gill <komalgill0310@gmail.com>
2022-01-30 17:23:09 -0500

    issue: when there is no vowel, app gives an error

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 let text = document.getElementById("paragraph-text");
 
-document.getElementById("count-word-vowel").addEventListener("click", (e) => {
-  e.preventDefault();
-  countVowels();
-  countWords();
-});
+displayNumberOfVowelAndWord();
+resetApp();
 
-document.getElementById("reset").addEventListener("click", (e) => {
-  e.preventDefault();
-resetPage();
-})
+function displayNumberOfVowelAndWord() {
+  document.getElementById("count-word-vowel").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (isTextBoxEmpty()) {
+      alert("Please type some text in Text Box!");
+    }
+    countVowels();
+    countWords();
+  });
+}
+
+function resetApp() {
+  document.getElementById("reset").addEventListener("click", (e) => {
+    e.preventDefault();
+    return isTextBoxEmpty() ? alert("Please type some text in Text Box!") : resetPage();
+  });
+}
+
+function isTextBoxEmpty() {
+  return document.getElementById("paragraph-text").value === "" ? true : false;
+}
 
 function countVowels() {
-  const vowelCount = text.value.match(/[aeiou]/gim).length;
+  const regEx = (/(?=[aeiou])|([aeiou])/gim);
+  const findVowels = text.value.match(regEx);
+  // if(findVowels){
+  //   console.log("hey");
+  // }
+  // else{
+  //   console.log("no match!");
+  // }
+  // const findVowels = text.value.match(/[aeiou]/gim);
+  const vowelCount = findVowels.length;
+// if(vowelCount === text.value.match(/[^aeiou]/gim)){
+//    console.log('hello');
+// }
   document.getElementById("vowel-count").textContent = `Contains ${vowelCount}vowels`;
 }
 
-function countWords(){
- let wordCount = text.value.match(/([^\s])+/gim).length;
- document.getElementById("word-count").textContent = `Contains ${wordCount}words`;
+function countWords() {
+  let wordCount = text.value.match(/([^\s])+/gim).length;
+  document.getElementById("word-count").textContent = `Contains ${wordCount}words`;
 }
 
-function resetPage(){
+function resetPage() {
   text.value = "";
   document.getElementById("vowel-count").textContent = "";
   document.getElementById("word-count").textContent = "";
